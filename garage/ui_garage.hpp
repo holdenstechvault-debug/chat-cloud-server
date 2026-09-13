49a326be3e05cbab21a33a2f0e95d77017f1c34d
holdenstechvault-debug <holdenstechvault@gmail.com>
2026-09-13 12:49:24 -0400

    Add MegaCode command buttons 1 2 3

--- garage/ui_garage.hpp
+++ garage/ui_garage.hpp
@@ -6,7 +6,6 @@
 #define __UI_LINEAR_GARAGE_H__
 
 #include "ui.hpp"
-#include "ui_transmitter.hpp"
 #include "transmitter_model.hpp"
 #include "app_settings.hpp"
 #include "radio_state.hpp"
@@ -26,7 +25,6 @@ class GarageView : public View {
    private:
     NavigationView& nav_;
 
-    // Linear MegaCode is 318 MHz OOK. Keep the normal Mayhem OOK sample rate.
     TxRadioState radio_state_{
         318000000,
         1750000,
@@ -42,31 +40,20 @@ class GarageView : public View {
     static constexpr uint32_t kRepeats = 6;
     static constexpr uint32_t kInterframeGapMs = 9;
 
-    // This package's own transmitter identity.
-    // Key layout:
-    //   bit 23: start bit = 1
-    //   bits 22..19: facility code
-    //   bits 18..3: transmitter number
-    //   bits 2..0: button
     static constexpr uint8_t kFacility = 0;
     static constexpr uint16_t kTransmitter = 44734;
-    static constexpr uint8_t kButton = 2;
-    static constexpr uint32_t kMegaCodeKey =
-        (1UL << 23) |
-        (static_cast<uint32_t>(kFacility) << 19) |
-        (static_cast<uint32_t>(kTransmitter) << 3) |
-        kButton;
 
-    size_t generate_frame();
-    void start_tx();
+    size_t generate_frame(uint8_t button);
+    void start_tx(uint8_t button);
     void stop_tx();
     void on_tx_progress(uint32_t progress, bool done);
+
+    uint8_t active_button_{0};
 
     Labels labels{
         {{2 * 8, 1 * 16}, "Linear MegaCode Garage", Theme::getInstance()->fg_light->foreground},
         {{2 * 8, 2 * 16}, "318.000 MHz - learned ID", Theme::getInstance()->fg_light->foreground},
-        {{2 * 8, 5 * 16}, "Press START to operate.", Theme::getInstance()->fg_light->foreground},
-        {{2 * 8, 6 * 16}, "Enroll once with LEARN.", Theme::getInstance()->fg_light->foreground},
+        {{2 * 8, 4 * 16}, "Try command buttons:", Theme::getInstance()->fg_light->foreground},
     };
 
     Text text_identity{
@@ -74,21 +61,28 @@ class GarageView : public View {
         "ID"
     };
 
+    Button button_1{
+        {16, 5 * 16, 64, 32},
+        "1"
+    };
+
+    Button button_2{
+        {88, 5 * 16, 64, 32},
+        "2"
+    };
+
+    Button button_3{
+        {160, 5 * 16, 64, 32},
+        "3"
+    };
+
     Text text_status{
-        {2 * 8, UI_POS_Y_BOTTOM(7), 160, 16},
+        {2 * 8, 8 * 16, 27 * 8, 16},
         "Ready"
     };
 
     ProgressBar progressbar{
-        {2 * 8, UI_POS_Y_BOTTOM(6), UI_POS_WIDTH_REMAINING(4), 16}
-    };
-
-    // lock=true keeps frequency/bandwidth fixed while leaving gain controls available.
-    TransmitterView tx_view{
-        (int16_t)UI_POS_Y_BOTTOM(4),
-        0,
-        15,
-        true
+        {2 * 8, 9 * 16, UI_POS_WIDTH_REMAINING(4), 16}
     };
 
     MessageHandlerRegistration message_handler_tx_progress{
